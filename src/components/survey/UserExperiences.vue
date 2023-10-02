27d9a58adcdb4e385dc247e0f050e69ef24915c9
wingskush <kthapa@deerhold.org>
2023-10-02 17:19:14 +0545

    feat: set up firebase, add forms to store and fetch survey result

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return { results: [] };
+  },
+  methods: {
+    loadExperiences() {
+      const results = [];
+      fetch(
+        'https://vue-http-demo-66abf-default-rtdb.asia-southeast1.firebasedatabase.app/survey.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
